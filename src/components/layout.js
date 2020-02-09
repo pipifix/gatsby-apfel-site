3910e676b0ccf11230cf5615e784c89077e0d999
Thomas Apfelbacher <apfelbachert@Thomass-MBP-2.fritz.box>
2020-02-09 10:24:44 +0100

    First commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -35,7 +35,7 @@ const Layout = ({ children }) => {
       >
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
+          © {new Date().getFullYear()}, Built with &hearts; and
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
